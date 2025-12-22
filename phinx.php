e77cadd080dff73d5e72d50c6b4b5fc9799d4a8d
Osmair Coelho <osmair.coelho@gmail.com>
2025-12-22 20:23:57 -0300

    criar arquivo .env

--- phinx.php
+++ phinx.php
@@ -1,43 +1,45 @@
 <?php
+require __DIR__ . '/vendor/autoload.php';
+
+if (file_exists(__DIR__ . '/.env')) {
+    $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+    $dotenv->load();
+}
 
 $db = include __DIR__ . '/config/db.php';
+
+list(
+    'driver' => $adapter,
+    'host' => $host,
+    'database' => $name,
+    'username' => $user,
+    'password' => $pass,
+    'charset' => $charset,
+    'collation' => $collation
+    ) = $db['default_connection'];
 
 return
 [
     'paths' => [
-        'migrations' => '%%PHINX_CONFIG_DIR%%/db/migrations',
-        'seeds' => '%%PHINX_CONFIG_DIR%%/db/seeds'
-    ],
-    'environments' => [
-        'default_migration_table' => 'phinxlog',
-        'default_environment' => 'development',
-        'production' => [
-            'adapter' => 'mysql',
-            'host' => 'localhost',
-            'name' => 'production_db',
-            'user' => 'root',
-            'pass' => '',
-            'port' => '3306',
-            'charset' => 'utf8',
+        'migrations' => [
+            __DIR__ . '/db/migrations'
         ],
-        'development' => [
-            'adapter' => 'mysql',
-            'host' => 'localhost',
-            'name' => 'development_db',
-            'user' => 'root',
-            'pass' => '',
-            'port' => '3306',
-            'charset' => 'utf8',
-        ],
-        'testing' => [
-            'adapter' => 'mysql',
-            'host' => 'localhost',
-            'name' => 'testing_db',
-            'user' => 'root',
-            'pass' => '',
-            'port' => '3306',
-            'charset' => 'utf8',
+        'seeds' => [
+            __DIR__ . '/db/seeds'
         ]
     ],
+    'environments' => [
+            'default_migration_table' => 'migrations',
+            'default_database' => 'default_connection',
+            'default_connection' => [
+                'adapter' => $adapter,
+                'host' => $host,
+                'name' => $name,
+                'user' => $user,
+                'pass' => $pass,
+                'charset' => $charset,
+                'collation' => $collation
+            ]
+            ],
     'version_order' => 'creation'
 ];
