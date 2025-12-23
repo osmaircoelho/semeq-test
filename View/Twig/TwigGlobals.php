cd7a24580972e3ca85f275850c6ccf4275332cdf
Osmair Coelho <osmair.coelho@gmail.com>
2025-12-23 10:25:28 -0300

    instaldo twig

---
+++ View/Twig/TwigGlobals.php
@@ -0,0 +1,31 @@
+<?php
+
+
+namespace Semeq\View\Twig;
+
+
+use Semeq\Auth\AuthInterface;
+
+class TwigGlobals extends \Twig_Extension implements \Twig_Extension_GlobalsInterface
+{
+    /**
+     * @var AuthInterface
+     */
+    private $auth;
+
+
+    /**
+     * TwigGlobals constructor.
+     */
+    public function __construct(AuthInterface $auth)
+    {
+        $this->auth = $auth;
+    }
+
+    public function getGlobals()
+    {
+        return [
+            'Auth' => $this->auth
+        ];
+    }
+}
---
+++ src/Plugins/ViewPlugin.php
@@ -0,0 +1,48 @@
+<?php
+declare(strict_types=1);
+
+namespace Semeq\Plugins;
+
+use Interop\Container\ContainerInterface;
+use Semeq\ServiceContainerInterface;
+//use Semeq\View\Twig\TwigGlobals;
+use Semeq\View\Twig\TwigGlobals;
+use Semeq\View\ViewRenderer;
+
+
+class ViewPlugin implements PluginInterface
+{
+
+    public function register(ServiceContainerInterface $container)
+    {
+        $container->addLazy(
+            'twig', function (ContainerInterface $container) {
+                $loader = new \Twig_Loader_Filesystem(__DIR__ .'/../../templates');
+                $twig = new \Twig_Environment($loader);
+
+                $auth = $container->get('auth');
+
+                $generator = $container->get('routing.generator');
+
+                $twig->addExtension(new TwigGlobals($auth));
+                $twig->addFunction(
+                    new \Twig_SimpleFunction(
+                        'route',
+                        function (string $name, array $params=[]) use ($generator) {
+                            return $generator->generate($name, $params);
+                        }
+                    )
+                );
+
+                return $twig;
+            }
+        );
+        $container->addLazy(
+            'view.renderer', function (ContainerInterface $container) {
+                $twigEnvironment = $container->get('twig');
+                return new ViewRenderer($twigEnvironment);
+            }
+        );
+    }
+
+}
